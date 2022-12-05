6bc0822314dfbd4a37f2aa97a078140fe4f862f2
Youssef4848 <youssefamallah00@gmail.com>
2022-12-05 10:56:37 +0100

    update

--- POO/ServiceCommande.h
+++ POO/ServiceCommande.h
@@ -8,24 +8,7 @@ namespace Services
 {
     ref class ServiceCommande
     {
-    public:
-        ServiceCommande();
 
-        DataSet^ listeCommande(String^ dataTableName);
-
-        //DataSet^ adressesClient(int idPersonne, String^ dataTableName);
-
-        //int ajouter(String^ nom, String^ prenom, array<String^>^ adresses);
-
-        //void modifier(int idPersonne, String^ nom, String^ prenom, array<String^>^ adresses);
-
-        //void supprimer(int idPersonne);
-
-    private:
-        Composants::Cad^ cad;
-        Mappage::Commande^ Commande;
-
-        DataSet^ dataSet;
     };
 }
 
